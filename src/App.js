c70e744a6132de978dea031287bb68167fad690f
malys@LAPTOP-DSQPC4U1 <alenamalyseva422@gmail.com>
2022-12-23 15:56:57 +0200

    nav and footer added,routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import MainPage from './pages/MainPage';
+import AboutPage from './pages/AboutPage';
+import CategoriesPage from './pages/CategoriesPage';
 
 function App() {
   return (
-    <div></div>
+    <Routes>
+      <Route path='/' element={<MainPage/>}/>
+      <Route path='/about' element={<AboutPage/>}/>
+      <Route path='/categories' element={<CategoriesPage/>}/>
+    </Routes>
   );
 }
 
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import s from './index.module.css'
+
+export default function Footer() {
+  return (
+    <div className={s.footer}>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Aliquid eius magnam adipisci est minus nam velit odio vitae 
+        sint at, excepturi eveniet molestias atque harum exercitationemut 
+        ex quisquam! Aliquid?
+    </div>
+  )
+}
